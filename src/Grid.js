e2b72497b37b5a98a9de52a886bc9068bb32d330
bob <novdavin1@gmail.com>
2022-02-11 09:40:51 +0700

    Finished making a grid

---
+++ src/Grid.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function Grid() {
+  return (
+    <div id="grid-Container">
+      <div id="grid">
+        <div className="X"></div>
+        <div className="O"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
